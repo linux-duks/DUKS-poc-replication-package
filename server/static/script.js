0e5d4600e4f70eabd7a7fc1742ab2fd3525d79ee
Arthur Pilone <art.pilone@gmail.com>
2025-06-09 17:58:12 -0300

    decouples plot func from available data
    Signed-off-by: Arthur Pilone <art.pilone@gmail.com>

--- server/static/script.js
+++ server/static/script.js
@@ -11,13 +11,17 @@ async function get_tags(){
     
     return json_tags
 }
+// -----------------------------------------------------------------------------------
+// Computing Sliding Window Data
+// -----------------------------------------------------------------------------------
+
 /**
  * Computes the sum of the LoC differences for a sliding window.
  * @param {*} commits 
  * @param {number} intervalLengthDays 
  * @returns [commit_dates, summed_pos_diffs, summed_neg_diffs, summed_total_diffs]
  */
-function sliding_window_diffs(commits, intervalLengthDays){
+function slidingWindowDiffs(commits, intervalLengthDays){
 
     let running_minus = commits[0]["deletions"];
     let running_plus = commits[0]["insertions"];
@@ -60,13 +64,13 @@ function sliding_window_diffs(commits, intervalLengthDays){
         date_points.push(thisDate);
         plus_points.push(running_plus);
         minus_points.push(running_minus);
-        acc_points.push(running_acc);
+        acc_points.push(Math.abs(running_acc));
     }
 
     return [date_points, plus_points, minus_points, acc_points]
 }
 
-function sliding_window_authors(commits, intervalLengthDays){
+function slidingWindowAuthors(commits, intervalLengthDays){
 
     let runningAuthors = {};
     let runningCommitters = {};
@@ -130,44 +134,94 @@ function sliding_window_authors(commits, intervalLengthDays){
     return [date_points, num_authors ,num_committers]
 }
 
-function plot_thing(commits){
+// -----------------------------------------------------------------------------------
+// Load Initial Data
+// -----------------------------------------------------------------------------------
+
+// Nothing like a good and old global variable
+var branchData = {
+    'leftDataPoints': [], // keys from 'allData dict' shown in the left y axis
+    'rightDataPoints' :[], // keys from 'allData dict' shown in the right y axis
+    'chosenData' : [], // keys from 'allData dict'
+    'allData': [], // dict{'<title>' : dict {'axisLabel': '', 'data': [] } } 
+    'commitDates': [], // xAxis for all traces
+}
+
+function setBranchData(newBranchData){
+    branchData = newBranchData;
+}
+
+function getBranchData(){
+    return branchData;
+}
 
 
-    WINDOW_RADIUS = 5
+/**
+ * Computes the sliding window data relative to the given list of commits.
+ * Updates the global variable 'branchData' with the new data.
+ * @param {*} commitList List of commits from the tree to be analyzed.
+ */
+function computeBranchData(commitList){
 
-    const contribs_results = sliding_window_authors(commits,WINDOW_RADIUS)
-    const diffs_results = sliding_window_diffs(commits,WINDOW_RADIUS)
-    
-    //for(commit of commits){
-    //    dates.push(commit["committer_date"])
-    //} 
+    WINDOW_RADIUS = 10
 
-    var contributorsTrace = {
-        x: contribs_results[0],
-        y: contribs_results[1],
-        type: 'lines',
-        yaxis: 'y',
-        name: 'Authors'
-    };
+	  // TODO: use extra attributions
+    commits = loadExtraAttributions(commitList)
 
-    var committersTrace = {
-        x: contribs_results[0],
-        y: contribs_results[2],
-        type: 'lines',
-        yaxis: 'y',
-        name: 'Committers',
+    const contribs_results = slidingWindowAuthors(commits,WINDOW_RADIUS)
+    const diffs_results = slidingWindowDiffs(commits,WINDOW_RADIUS)
+
+    const dates = contribs_results[0];
+    const allData = {
+        'Authors' : {'axisLabel': 'Contributors', 'data': contribs_results[1]},
+        'Commiters' : {'axisLabel': 'Contributors', 'data': contribs_results[2]},
+        'LoC Added' : {'axisLabel': 'LoC', 'data': diffs_results[1]},
+        'LoC Removed' : {'axisLabel': 'LoC', 'data': diffs_results[2]},
+        'LoC Changes' : {'axisLabel': 'LoC', 'data': diffs_results[3]},
     }
 
-    var diffTrace = {
-        x: diffs_results[0],
-        y: diffs_results[1],
-        type: 'lines',
-        yaxis: 'y2',
-        name: 'LoC Changes'
+    const newBranchData = {
+        'leftDataPoints': ['Authors','Commiters'],
+        'rightDataPoints':  ['LoC Changes'],
+        'allData': allData,
+        'commitDates': dates
     };
 
+    setBranchData(newBranchData);
+}
 
-    var data = [contributorsTrace,committersTrace,diffTrace];
+function plot_thing(){
+
+    const branchData = getBranchData()
+
+    const xData = branchData['commitDates'];
+
+    var data = [];
+
+    let leftAxisLabel; // I'm supposing these labels don't change
+    let rightAxisLabel;
+
+    for(leftKey of branchData['leftDataPoints']){
+        data.push({
+            x: xData,
+            y: branchData['allData'][leftKey]['data'],
+            type: 'lines',
+            yaxis: 'y',
+            name: leftKey
+        })
+        leftAxisLabel = branchData['allData'][leftKey]['axisLabel'];
+    }
+
+    for(rightKey of branchData['rightDataPoints']){
+        data.push({
+            x: xData,
+            y: branchData['allData'][rightKey]['data'],
+            type: 'lines',
+            yaxis: 'y2',
+            name: rightKey
+        })
+        rightAxisLabel = branchData['allData'][rightKey]['axisLabel'];
+    }
 
     var layout = {
         title: 'Test plot with Contributor and Diff Data',
@@ -175,10 +229,10 @@ function plot_thing(commits){
             title: 'Date'
         },
         yaxis: {
-            title: 'Contributors'
+            title: leftAxisLabel
         },
         yaxis2: {
-            title: 'LoC Altered',
+            title: rightAxisLabel,
             overlaying: 'y',
             side: 'right',
             anchor: 'x',
@@ -208,3 +262,19 @@ function map_tags(tags){
 }
 // WIP
 get_tags().then( (tags) => map_tags(tags))
+
+/**
+ * Parses the 'attributions' field of every commit object of a list
+ * into a json object.
+ * @param {*} commits 
+ * @returns commits
+ */
+function loadExtraAttributions(commits){
+    for(commit of commits){
+        commit["attributions"] = JSON.parse(commit["attributions"]);
+    }
+    return commits
+}
+
+
+get_commits().then( (commits) => {computeBranchData(commits);plot_thing();})
